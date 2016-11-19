e16841893b267fd14107bf79bf17fad9aa3a2190
Thom <snkenjoi@gmail.com>
2016-11-19 13:20:55 +0000

    acube edges output

--- state/acube.js
+++ state/acube.js
@@ -1,30 +1,24 @@
 // UF UR UB UL DF DR DB DL FR FL BR BL
-// UFR URB UBL ULF DRF DFL DLB DBR 
+// UFR URB UBL ULF DRF DFL DLB DBR
 
 export default function (cube) {
     let { centres, edges, corners } = cube;
 
     let faces = [...'UBRFLD'];
 
-    // I don't actually know why this works >_<
-    // let letters = centres.map((d)=>faces[d]);
-    // let lettersMap = centres.map((d)=>letters[d]);
-    // let lettersRemap = centres.map((d)=>lettersMap[d]);
+    // kSim to acube position translation
+    let pos = {
+        edges: [2, 1, 0, 3, 10, 9, 8, 11, 5, 6, 4, 7],
+        corners: []
+    };
 
-    // let pos = {
-    //     edges: [2, 1, 0, 3, 10, 9, 8, 11]
-    // };
-
-    // let asd = pos.edges.map((d) => {
-    //     return edges[d].map((e) => lettersRemap[e]);
-    // });
-
-    // U,FF,RU,RU,L\
-
-    return JSON.stringify({
-        // centres,
-        // letters,
-        // edges,
-        // asd: asd.map((d) => d.join``).join` `
+    // face index is the _index_ the number appears in the centres array
+    let edgesOut = pos.edges.map((d) => {
+        return edges[d].map((e) => faces[centres.indexOf(e)]);
     });
+
+    return {
+        centres,
+        edges: edgesOut.map((d) => d.join``).join` `
+    };
 }
