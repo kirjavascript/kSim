94715f1e2d397e554ba01c1e1d779e3edd0860d1
Thom <snkenjoi@gmail.com>
2016-12-15 22:52:24 +0000

    .

--- solver/check-solved.js
+++ solver/check-solved.js
@@ -4,20 +4,23 @@ let { centres, edges, corners } = solved;
 function checkSolved(position) {
 
     // edges
-    for (let i=0;i<12;i++) {
-        if (position.edges[i][0] != edges[i][0])
+    // only check 11
+    for (let i=0;i<11;i++) {
+        if (position.edges[i][0] !== edges[i][0])
             return 0;
-        else if (position.edges[i][1] != edges[i][1])
+        else if (position.edges[i][1] !== edges[i][1])
             return 0;
     }
     //corners
-    for (let i=0;i<8;i++) {
-        if (position.corners[i][0] != corners[i][0])
+    // only check 7
+    for (let i=0;i<7;i++) {
+        if (position.corners[i][0] !== corners[i][0])
             return 0;
-        else if (position.corners[i][1] != corners[i][1])
+        else if (position.corners[i][1] !== corners[i][1])
             return 0;
-        else if (position.corners[i][2] != corners[i][2])
-            return 0;
+        // 3rd sticker is implied
+        // else if (position.corners[i][2] !== corners[i][2])
+        //     return 0;
     }
 
     return 1;
--- solver/worker.js
+++ solver/worker.js
@@ -5,25 +5,26 @@ import { checkSolved } from './check-solved';
 
 // split permutation/orientation
 
+// TypedArrays
+// Symbol.for
+
 ~function search(maxDepth) {
 
-    let position = {'edges':[[0,3],[3,2],[4,0],[0,2],[3,5],[5,2],[1,0],[1,4],[5,1],[1,2],[4,3],[5,4]],'corners':[[4,3,5],[3,2,5],[4,0,3],[0,2,3],[4,1,0],[1,5,2],[2,0,1],[5,1,4]]};
-
+    let position = {'edges':[[0,4],[5,2],[3,4],[5,3],[3,2],[0,1],[1,2],[1,4],[5,1],[0,2],[0,3],[5,4]],'corners':[[5,3,2],[4,1,0],[3,4,0],[0,1,2],[0,2,3],[4,3,5],[5,2,1],[5,1,4]]};
 
     for (let i=0;i<maxDepth+1;i++) {
-
         postMessage(`searching depth ${i}`);
         tree([position], [], i);
-
     }
+    postMessage('done');
 
-} (4);
+} (6);
 
 function tree(stack, moves, depth) {
 
     let lastPosition = stack[stack.length-1];
 
-    if (depth == 0) {
+    if (depth === 0) {
         // only check solutions at the leaf nodes
         if (checkSolved(lastPosition)) {
             postMessage(formatMoves(moves));
@@ -31,7 +32,7 @@ function tree(stack, moves, depth) {
         return;
     }
 
-    let lastMove = (!moves.length?'root':moves[moves.length-1].move);
+    let lastMove = (moves.length===0?'root':moves[moves.length-1].move);
     let allowedMoves = allowed[lastMove];
 
     for (let i=0;i<allowedMoves.length;i++) {
